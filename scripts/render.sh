0fc091619d3f79ed05a340685dc1b06dd04fc8cf
mocaffy <mocaffy@gmail.com>
2023-02-15 16:30:23 +0900

    ♻️ リファクタ - max_tabs の設定を不要に - window-status-format に設定される文字列がより簡潔になるように

--- scripts/render.sh
+++ scripts/render.sh
@@ -25,10 +25,9 @@ done
 icon_format=""
 
 if [ ${#auto_icons[@]} -gt 0 ]; then
-	for ((I = 0, J = $base_index; I < $max_tabs; I++, J++)); do
-		K=$((I % ${#auto_icons[@]}))
-		is_target_format="?#{==:#I,$J}"
-		icon_format="#{$is_target_format,${auto_icons[$K]} ,$icon_format}"
+	for ((I = 0; I < ${#auto_icons[@]}; I++)); do
+		is_target_format="?#{==:#{e|m|:#I,${#auto_icons[@]}},$I}"
+		icon_format="#{$is_target_format,${auto_icons[$I]} ,$icon_format}"
 	done
 fi
 
@@ -41,44 +40,46 @@ fi
 window_status_format=""
 window_status_current_format=""
 
-for ((I = 0, J = $base_index; I < $max_tabs; I++, J++)); do
-	K=$((I % ${#auto_colors[@]}))
-	color_format=${auto_colors[$K]}
+color_format=""
+if [ ${#auto_colors[@]} -gt 0 ]; then
+	for ((I = 0; I < ${#auto_colors[@]}; I++)); do
+		is_target_format="?#{==:#{e|m|:#I,${#auto_colors[@]}},$I}"
+		color_format="#{$is_target_format,${auto_colors[$I]} ,$color_format}"
+	done
+fi
 
-	if [ ${#manual_colors[@]} -gt 0 ]; then
-		for ((L = 0; L < ${#manual_colors[@]}; L++)); do
-			color_format="#{${manual_colors[$L]} ,$color_format}"
-		done
-	fi
+if [ ${#manual_colors[@]} -gt 0 ]; then
+	for ((I = 0; I < ${#manual_colors[@]}; I++)); do
+		color_format="#{${manual_colors[$I]} ,$color_format}"
+	done
+fi
 
-	is_target_format="?#{==:#I,$J}"
-	is_first_format="?#{!=:#I,${window_ids[0]}}"
-	is_last_format="?#{!=:#I,${window_ids[$(($window_count - 1))]}}"
+is_first_format="?#{!=:#I,${window_ids[0]}}"
+is_last_format="?#{!=:#I,${window_ids[$(($window_count - 1))]}}"
 
-	tab_style=${style_tab//\#C/$color_format}
-	tab_before_format="#{$is_first_format,$tab_before,$tab_before_first}"
-	tab_before_format=${tab_before_format//\#C/$color_format}
-	tab_after_format="#{$is_last_format,$tab_after,$tab_after_last}"
-	tab_after_format=${tab_after_format//\#C/$color_format}
+tab_style=${style_tab//\#C/$color_format}
+tab_before_format="#{$is_first_format,$tab_before,$tab_before_first}"
+tab_before_format=${tab_before_format//\#C/$color_format}
+tab_after_format="#{$is_last_format,$tab_after,$tab_after_last}"
+tab_after_format=${tab_after_format//\#C/$color_format}
 
-	window_status_format+="#{$is_target_format,$tab_style$tab_before_format$tab_style,}"
-	icon_style=${style_tab_icon//\#C/$color_format}
-	window_status_format+="#{$is_target_format,$icon_style$icon_format,}"
-	title_style=${style_tab_title//\#C/$color_format}
-	window_status_format+="#{$is_target_format,$title_style$tab_title$tab_after_format,}"
+window_status_format+="$tab_style$tab_before_format$tab_style"
+icon_style=${style_tab_icon//\#C/$color_format}
+window_status_format+="$icon_style$icon_format"
+title_style=${style_tab_title//\#C/$color_format}
+window_status_format+="$title_style$tab_title$tab_after_format"
 
-	tab_style=${style_tab_active//\#C/$color_format}
-	tab_active_before_format="#{$is_first_format,$tab_active_before,$tab_active_before_first}"
-	tab_active_before_format=${tab_active_before_format//\#C/$color_format}
-	tab_active_after_format="#{$is_last_format,$tab_active_after,$tab_active_after_last}"
-	tab_active_after_format=${tab_active_after_format//\#C/$color_format}
+tab_style=${style_tab_active//\#C/$color_format}
+tab_active_before_format="#{$is_first_format,$tab_active_before,$tab_active_before_first}"
+tab_active_before_format=${tab_active_before_format//\#C/$color_format}
+tab_active_after_format="#{$is_last_format,$tab_active_after,$tab_active_after_last}"
+tab_active_after_format=${tab_active_after_format//\#C/$color_format}
 
-	window_status_current_format+="#{$is_target_format,$tab_style$tab_active_before_format$tab_style,}"
-	icon_style=${style_tab_active_icon//\#C/$color_format}
-	window_status_current_format+="#{$is_target_format,$icon_style$icon_format,}"
-	title_style=${style_tab_active_title//\#C/$color_format}
-	window_status_current_format+="#{$is_target_format,$title_style$tab_active_title$tab_active_after_format,}"
-done
+window_status_current_format+="$tab_style$tab_active_before_format$tab_style"
+icon_style=${style_tab_active_icon//\#C/$color_format}
+window_status_current_format+="$icon_style$icon_format"
+title_style=${style_tab_active_title//\#C/$color_format}
+window_status_current_format+="$title_style$tab_active_title$tab_active_after_format"
 
 tmux set-window-option -g window-status-format ""
 tmux set-window-option -g window-status-current-format ""
