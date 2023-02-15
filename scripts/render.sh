ffebc626de6f336251cef18c70e362c9c7fe2006
mocaffy <mocaffy@gmail.com>
2023-02-15 19:54:11 +0900

    :art: 似た表現を関数に

--- scripts/render.sh
+++ scripts/render.sh
@@ -78,39 +78,41 @@ is_first_format="?#{!=:#I,${window_ids[0]}}"
 is_last_format="?#{!=:#I,${window_ids[$(($window_count - 1))]}}"
 
 #######################################
-# 置換系関数
+# 文字列生成系関数
 #######################################
 rep-cf() { v=$1; echo ${v//\#C/$color_format}; }
+
+mk-wsf() {
+	echo "
+		$default_style
+		$tab_before_format$default_style
+		$icon_style$icon_format$default_style
+		$title_style$tab_title_format$default_style
+		$tab_after_format
+	"
+}
 
 #######################################
 # 通常タブのフォーマットを作成
 #######################################
-tab_style=$(rep-cf $style_tab)
+default_style=$(rep-cf $style_tab)
 icon_style=$(rep-cf $style_tab_icon)
 title_style=$(rep-cf $style_tab_title)
-
 tab_before_format=$(rep-cf "#{$is_first_format,$tab_before,$tab_before_first}")
 tab_after_format=$(rep-cf "#{$is_last_format,$tab_after,$tab_after_last}")
-
-window_status_format="$tab_style"
-window_status_format+="$tab_style$tab_before_format$tab_style"
-window_status_format+="$icon_style$icon_format$tab_style"
-window_status_format+="$title_style$tab_title$tab_after_format"
+tab_title_format=$tab_title
+window_status_format=$(mk-wsf)
 
 #######################################
 # アクティブなタブのフォーマットを作成
 #######################################
-tab_style=$(rep-cf $style_tab_active)
+default_style=$(rep-cf $style_tab_active)
 icon_style=$(rep-cf $style_tab_active_icon)
 title_style=$(rep-cf $style_tab_active_title)
-
-tab_active_before_format=$(rep-cf "#{$is_first_format,$tab_active_before,$tab_active_before_first}")
-tab_active_after_format=$(rep-cf "#{$is_last_format,$tab_active_after,$tab_active_after_last}")
-
-window_status_current_format="$tab_style"
-window_status_current_format+="$tab_active_before_format$tab_style"
-window_status_current_format+="$icon_style$icon_format$tab_style"
-window_status_current_format+="$title_style$tab_active_title$tab_active_after_format"
+tab_before_format=$(rep-cf "#{$is_first_format,$tab_active_before,$tab_active_before_first}")
+tab_after_format=$(rep-cf "#{$is_last_format,$tab_active_after,$tab_active_after_last}")
+tab_title_format=$tab_active_title
+window_status_current_format=$(mk-wsf)
 
 #######################################
 # グローバルな設定を削除
